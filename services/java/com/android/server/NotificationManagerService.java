4a93da1a8acd7912db7ad310f017af95fa6722e6
timpohladthomas <timpohladthomas@gmail.com>
2014-02-07 14:34:49 +0700

    [1/2] Base: Battery Saver mode
    Concept:
    http://jira.omnirom.org/browse/CONCEPT-24
    
    introduce battery saver mode
    this based from what user want or what user do to saving the battery life
    this include:
    1. Automatically switch mobile network into what user choice and also disable mobile data IF wifi connected to an AP
    2. Automatically switch mobile network and disable mobile data IF battery low (battery level can configure)
    3. Reduce screen brightness based low battery (this feature will disabled if Auto brightness mode enabled)
    4. Not changing network IF on Call or Usb tether enabled or WifiAP enabled
    5. Not changing network IF there is Download/upload progress
    6. Support For Both GSM and CDMA devices (need someone to test for CDMA devices)
    7. every condition has an option :D
    8. Restore all network configuration if battery saver mode disabled
    9. Add Time Range to start battery saver mode
    10. allow device without mobile data support, to using this feature
    11. fix inconsistent battery low level
    12. rewrite to better performance
    13. modified the controller to support battery saver mode
    14. add bluetooth, gps, wifi, and mobile data (if support) to be disabled while power saving activated
    15. fix build QuickSettings because of LocationController change
    16. rewrite to better performance
    17. fix brightness value
    18. rewrite (again) to allow add more saving mode
    19. do delay changing only for POWER SAVING not NORMAL mode
    20. add Cpu power saving mode
    21. fix Cpu freq value
    22. fix call saving state for non user configuration mode
    23. fix NPE
    24. add kill all task
    25. add disable sync
    26. add disable vibrate
    27. add disable led
    28. add notification :)
    29. finally fix sync interval :D
    
    preview:
    http://jira.omnirom.org/secure/attachment/10394/Screenshot_2014-03-07-19-54-57.png
    http://jira.omnirom.org/secure/attachment/10395/Screenshot_2014-03-07-19-55-56.png
    http://jira.omnirom.org/secure/attachment/10396/Screenshot_2014-03-07-19-56-06.png
    http://jira.omnirom.org/secure/attachment/10397/Screenshot_2014-03-07-19-56-14.png
    http://jira.omnirom.org/secure/attachment/10398/Screenshot_2014-03-07-19-56-27.png
    http://jira.omnirom.org/secure/attachment/10399/Screenshot_2014-03-07-19-56-34.png
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettings.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsModel.java
    
    Change-Id: I9d86ef1425228e11cadd85d37c5b6e8b20e50fb0
    
    Conflicts:
    	packages/SystemUI/res/values/custom_strings.xml
    	packages/SystemUI/src/com/android/systemui/BootReceiver.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettings.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsModel.java
    	packages/SystemUI/src/com/android/systemui/statusbar/policy/BatteryController.java
    	packages/SystemUI/src/com/android/systemui/statusbar/policy/BluetoothController.java
    	services/java/com/android/server/NotificationManagerService.java
    
    Conflicts:
    	core/java/android/content/Intent.java
    	packages/SystemUI/res/values/ose_strings.xml
    	services/java/com/android/server/NotificationManagerService.java
    
    Conflicts:
    	packages/SystemUI/res/values/liquid_strings.xml
    	services/java/com/android/server/NotificationManagerService.java
    
    Conflicts:
    	services/java/com/android/server/NotificationManagerService.java

--- services/java/com/android/server/NotificationManagerService.java
+++ services/java/com/android/server/NotificationManagerService.java
@@ -93,7 +93,6 @@ import java.lang.reflect.Array;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Calendar;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
@@ -169,6 +168,7 @@ public class NotificationManagerService extends INotificationManager.Stub
     private boolean mScreenOn = true;
     private boolean mWasScreenOn = true;
     private boolean mInCall = false;
+    private boolean mBatterySaverDisableLED = false;
     private boolean mNotificationPulseEnabled;
     private HashMap<String, NotificationLedValues> mNotificationPulseCustomLedValues;
     private Map<String, String> mPackageNameMappings;
@@ -1299,7 +1299,8 @@ public class NotificationManagerService extends INotificationManager.Stub
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.QUIET_HOURS_DIM),
                     false, this, UserHandle.USER_ALL);
-
+            resolver.registerContentObserver(Settings.Global.getUriFor(
+                    Settings.Global.BATTERY_SAVER_LED_DISABLE), false, this, UserHandle.USER_ALL);
             update(null);
         }
 
@@ -1342,6 +1343,9 @@ public class NotificationManagerService extends INotificationManager.Stub
             if (uri == null || ENABLED_NOTIFICATION_LISTENERS_URI.equals(uri)) {
                 rebindListenerServices();
             }
+
+            mBatterySaverDisableLED = Settings.Global.getInt(resolver,
+                    Settings.Global.BATTERY_SAVER_LED_DISABLE, 0) != 0;
         }
     }
 
@@ -2340,8 +2344,7 @@ public class NotificationManagerService extends INotificationManager.Stub
     }
 
     // lock on mNotificationList
-    private void updateLightsLocked()
-    {
+    private void updateLightsLocked() {
         // handle notification lights
         if (mLedNotification == null) {
             // get next notification, if any
@@ -2353,7 +2356,7 @@ public class NotificationManagerService extends INotificationManager.Stub
 
         // Don't flash while we are in a call, screen is
         // on or we are in quiet hours with light dimmed
-        if (mLedNotification == null || mInCall || mScreenOn
+        if (mBatterySaverDisableLED || mLedNotification == null || mInCall || mScreenOn
                 || (QuietHoursHelper.inQuietHours(mContext, Settings.System.QUIET_HOURS_DIM))) {
             mNotificationLight.turnOff();
         } else if (mNotificationPulseEnabled) {
