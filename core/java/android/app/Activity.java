340f23cde04617fa931475b368c5a4366d129809
David96 <xD_e-mail@gmx.de>
2014-04-16 21:04:25 +0200

    HALO: make windows moveable and scaleable [WIP]
    Change-Id: I8c3118f6d548dc70f186454fb8d15a4bae9bbeb0
    
    Conflicts:
    	core/java/android/app/Activity.java

--- core/java/android/app/Activity.java
+++ core/java/android/app/Activity.java
@@ -5317,11 +5317,16 @@ public class Activity extends ContextThemeWrapper
             mWindow.setCloseOnTouchOutsideIfNotSet(false);
             mWindow.setGravity(Gravity.TOP | Gravity.LEFT);
 
+            //if (this instanceof LayerActivity || android.os.Process.myUid() == android.os.Process.SYSTEM_UID) {
             int flags = WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL;
                 mWindow.setFlags(flags, flags);
-            WindowManager.LayoutParams params = mWindow.getAttributes();
-            params.alpha = 1f;
-            mWindow.setAttributes(params);
+                WindowManager.LayoutParams params = mWindow.getAttributes();
+                params.alpha = 1f;
+                //params.dimAmount = 0.25f;
+                //params.x = 10;
+                //params.y = 10;
+                mWindow.setAttributes(params);
+            //}
 
             // Scale it
             scaleFloatingWindow(context);
